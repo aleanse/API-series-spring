169fd8242bc020de3b4c54a4cad8d2f88165d955
aleanse <aleanse_54@hotmail.com>
2024-03-20 10:45:20 -0300

    daks

--- src/main/java/br/com/aleanse/clonenetflix/CloneNetflixApplication.java
+++ src/main/java/br/com/aleanse/clonenetflix/CloneNetflixApplication.java
@@ -1,13 +1,20 @@
 package br.com.aleanse.clonenetflix;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloneNetflixApplication {
+public class CloneNetflixApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CloneNetflixApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ola");
+	}
 }
