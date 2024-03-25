c53ed6aab25a67bec811cbfe6fbc93e16962df03
aleanse <aleanse_54@hotmail.com>
2024-03-25 11:57:25 -0300

    desserializando

--- src/main/java/br/com/aleanse/clonenetflix/CloneNetflixApplication.java
+++ src/main/java/br/com/aleanse/clonenetflix/CloneNetflixApplication.java
@@ -1,6 +1,8 @@
 package br.com.aleanse.clonenetflix;
 
+import br.com.aleanse.clonenetflix.model.DadoSerie;
 import br.com.aleanse.clonenetflix.service.ConsumoApi;
+import br.com.aleanse.clonenetflix.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,8 +19,12 @@ public class CloneNetflixApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?i=tt3896198&apikey=7c3783a2");
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=the+flash&apikey=7c3783a2");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadoSerie dados = conversor.obterDados(json,DadoSerie.class);
+		System.out.println(dados);
+
 
 
 	}
