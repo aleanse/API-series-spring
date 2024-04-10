3a122cd57d1d10119ecc90e96c09e6c1123c0f97
aleanse <aleanse_54@hotmail.com>
2024-04-10 14:03:14 -0300

    sas

--- src/main/java/br/com/aleanse/clonenetflix/CloneNetflixApplication.java
+++ src/main/java/br/com/aleanse/clonenetflix/CloneNetflixApplication.java
@@ -1,11 +1,8 @@
 package br.com.aleanse.clonenetflix;
-
 import br.com.aleanse.clonenetflix.model.DadoSerie;
-<<<<<<< HEAD
 import br.com.aleanse.clonenetflix.model.DadosTemporadas;
-=======
 import br.com.aleanse.clonenetflix.model.DadosEpisodio;
->>>>>>> 4d1ad4a2db65c027cef4cf0f1e7c3e1f4ceabd3f
+import br.com.aleanse.clonenetflix.principal.Principal;
 import br.com.aleanse.clonenetflix.service.ConsumoApi;
 import br.com.aleanse.clonenetflix.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -26,28 +23,12 @@ public class CloneNetflixApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi consumoApi = new ConsumoApi();
-<<<<<<< HEAD
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=the+flash&apikey=7c3783a2");
-		ConverteDados conversor = new ConverteDados();
-		DadoSerie dados = conversor.obterDados(json,DadoSerie.class);
-		System.out.println(dados);
-		List<DadosTemporadas> temporadas = new ArrayList<>();
-		for (int i = 1; i<=dados.totalTemporadas(); i++){
-			 json = consumoApi.obterDados("http://www.omdbapi.com/?t=the+flash&season="+i+"&apikey=7c3783a2");
-			 DadosTemporadas dadosTemporadas = conversor.obterDados(json,DadosTemporadas.class);
-			 temporadas.add(dadosTemporadas);
-		}
-		temporadas.forEach(System.out::println);
-=======
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=supernatural&apikey=7c3783a2");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadoSerie dados = conversor.obterDados(json,DadoSerie.class);
-		json = consumoApi.obterDados("http://www.omdbapi.com/?t=supernatural&season=3&episode=5&apikey=7c3783a2");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
->>>>>>> 4d1ad4a2db65c027cef4cf0f1e7c3e1f4ceabd3f
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+
+
+
 
 
 
--- src/main/java/br/com/aleanse/clonenetflix/principal/Principal.java
+++ src/main/java/br/com/aleanse/clonenetflix/principal/Principal.java
@@ -1,19 +1,59 @@
 package br.com.aleanse.clonenetflix.principal;
 
+import br.com.aleanse.clonenetflix.model.DadoSerie;
+import br.com.aleanse.clonenetflix.model.DadosEpisodio;
+import br.com.aleanse.clonenetflix.model.DadosEpisodios;
+import br.com.aleanse.clonenetflix.model.DadosTemporadas;
 import br.com.aleanse.clonenetflix.service.ConsumoApi;
+import br.com.aleanse.clonenetflix.service.ConverteDados;
+import org.apache.logging.log4j.util.PropertySource;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
-    private  ConsumoApi consumo = new ConsumoApi();
-    private final String ENDEREÇO = "http://www.omdbapi.com/?t=";
-    private final String API_KEY = "7c3783a2";
-    public void exibeMenu(){
-        System.out.println("digite o nome da serie");
-        var nomeSerie = leitura.nextLine();
-        var json = consumo.obterDados( ENDEREÇO + nomeSerie.replace(" ","+") + API_KEY);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadoSerie dados = conversor.obterDados(json, DadoSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporadas> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<=dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+            DadosTemporadas dadosTemporada = conversor.obterDados(json, DadosTemporadas.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
+//        for(int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for(int j = 0; j< episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+        List<DadosEpisodios> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed())
+                .limit(5).forEach(System.out::println);
     }
 
 
