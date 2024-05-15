e2ec2da6a6cfd4cc5b0fd92299f4031978bbbe03
aleanse <aleanse_54@hotmail.com>
2024-05-15 16:34:53 -0300

    adk

--- src/main/java/com/aleanse/clonenetflix/CloneNetflixApplication.java
+++ src/main/java/com/aleanse/clonenetflix/CloneNetflixApplication.java
@@ -1,13 +1,22 @@
 package com.aleanse.clonenetflix;
 
+import com.aleanse.clonenetflix.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloneNetflixApplication {
+public class CloneNetflixApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CloneNetflixApplication.class, args);
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		String json = consumoApi.obterDados();
+
 	}
 
 }
